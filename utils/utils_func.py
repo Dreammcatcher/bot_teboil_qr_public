b41af5055cbd3790f190af21956cdef7284e69c9
Dreamcatcher <gal1989@mail.ru>
2023-07-07 01:31:44 +0300

    complete

--- utils/utils_func.py
+++ utils/utils_func.py
@@ -1,3 +1,4 @@
+import random
 from io import BytesIO
 from models.model import session, Teboil, UserId
 import qrcode
@@ -10,12 +11,23 @@ async def create_qr_after_update(nomer, balance):
     """
     qrimg = qrcode.make(nomer)
     img = Image.open('images/com.teboil.azs_empty2.png')
-    img2 = Image.open('images/com.teboil.azs_empty_main.png')
+    img2 = Image.open('images/com.teboil.azs_empty_main_new.png')
 
     font = ImageFont.truetype("images/CriqueGroteskDisplay-BlackIt.ttf", 70)
+    font_main_window = ImageFont.truetype("images/roboto-medium.ttf", 93)
+    font_num_kart = ImageFont.truetype("images/roboto-medium.ttf", 35)
+
     drawer = ImageDraw.Draw(img)
+    drawer_main = ImageDraw.Draw(img2)
 
     drawer.text((690, 678), str(balance), font=font, fill='white', stroke_width=0)
+    if len(str(balance)) == 4:
+        drawer_main.text((85, 1670), str(balance), font=font_main_window, fill='black', stroke_width=0)
+    else:
+        drawer_main.text((125, 1670), str(balance), font=font_main_window, fill='black', stroke_width=0)
+    nomer = str(nomer)
+    nomer = f'{nomer[:4]} {nomer[4:8]} {random.randint(1000, 9999)} {random.randint(1000, 9999)}'
+    drawer_main.text((603, 880), str(nomer), font=font_num_kart, fill='white', stroke_width=0)
 
     width, height = qrimg.size
     barcod = qrimg.crop((37, 37, width - 37, height - 37))
